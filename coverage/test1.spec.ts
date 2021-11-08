9b6f7e5061d47ead34e47b45595174f2b5a7e68d
Gal <galsened@gmail.com>
2021-11-08 14:50:27 +0200

    init

---
+++ coverage/test1.spec.ts
@@ -0,0 +1,19 @@
+
+
+import { By } from 'selenium-webdriver';
+import { Builder } from '@tpio/javascript-opensdk';
+
+export const simpleTest = async (): Promise<void> => {
+    const driver = await new Builder().forBrowser('chrome').build();
+
+    await driver.get('https://example.testproject.io/web/');
+    await driver.findElement(By.css('#name')).sendKeys('John Smith');
+    await driver.findElement(By.css('#password')).sendKeys('12345');
+    await driver.findElement(By.css('#login')).click();
+
+    const passed = await driver.findElement(By.css('#logout')).isDisplayed();
+
+    console.log(passed ? 'Test Passed' : 'Test Failed');
+
+    await driver.quit();
+};
